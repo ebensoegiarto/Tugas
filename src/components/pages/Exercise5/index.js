25e7becf4458019bfff45a4dc123da1e3bbbc3b9
ebensoegiarto <s11810124@student.unklab.ac.id>
2021-04-20 16:30:22 +0800

    commit ke 5

---
+++ src/components/pages/Exercise5/index.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import {Button, Form, Container} from 'react-bootstrap';
+import Inputs from '../../atoms/Input';
+import Axios from 'axios';
+
+
+const Exercise5 = () => {
+    const [email, setEmail] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const saveData = () => {
+      const data = {
+        email: email,
+        first_name: firstName,
+        last_name: lastName,
+        avatar: 'https://reqres.in/img/faces/9-image.jpg',
+      };
+      Axios.post('http://localhost:3004/users', data);
+    };
+
+    return (
+    <div>
+      <div style={{display: "flex"}}>
+      <Container style={{flex: "1 0 1px"}}/>
+      <Container style={{flex: "4 0 1px"}}>
+        <br/>
+        <h3>Tugas 5</h3>
+      <Form>
+      <Inputs inputTitle="Email" placeholder="masukkan email" value ={email} onChange={(e)=>setEmail(e.target.value)}/>
+      <Inputs inputTitle="First Name" placeholder="masukkan first name" value ={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
+      <Inputs inputTitle="Last Name" placeholder="masukkan last name" value ={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+      <br/>
+      <Button variant="primary" type="submit" onClick={(e)=>{
+          e.preventDefault()
+          console.log(email, firstName, lastName)
+          saveData();
+        }}> 
+      </Button> 
+      </Form>
+      </Container>
+      <Container style={{flex: "1 0 1px"}}/>
+    </div>
+    </div>
+    )
+}
+
+export default Exercise5
